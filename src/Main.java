e41e7cadd87f7bdeb143199886be6724eb7ed98c
siddiqov <siddiqov@gmail.com>
2025-03-31 15:51:22 +0100

    Added add() methid

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World, Welcome to Java");
+        System.out.println("Sum: " + add(3,7));
     }
+public static int add(int a, int b)
+ {return a+b;}
 }
